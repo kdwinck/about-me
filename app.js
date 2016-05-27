b15e33b69047e0e9c9043e0d5edd2a027ac03864
Kyle Winckler <kyle.winckler@gmail.com>
2016-05-26 17:51:59 -0700

    linked js file -- created if statement for quesiton 1

---
+++ app.js
@@ -0,0 +1,7 @@
+var questionOne = prompt("Was I born in North Dakota?").toLowerCase();
+
+if (questionOne === 'yes' || questionOne === "y") {
+  alert("Wrong! I was born in South Dakota.");
+} else {
+  alert("Good Job! You're off to a good start.");
+}
