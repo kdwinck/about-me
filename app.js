85fbb1ce732a6cac3212e0e6674f9d5c7912f88d
Kyle Winckler <kyle.winckler@gmail.com>
2016-05-27 12:30:56 -0700

    made questions into functions -- made variable names accurate

--- app.js
+++ app.js
@@ -1,41 +1,54 @@
 var userName = prompt("Identify yourself user?"); // This will get the users name
 
-var questionOne = prompt("Was I born in North Dakota? (Please answer yes or no)").toLowerCase();
+var questionOne = function() {
+  var answerOne = prompt("Was I born in North Dakota? (Please answer yes or no)").toLowerCase();
+  if (answerOne === 'no' || answerOne === "n") {
+    alert("Good Job " + userName + "! You're off to a good start.");
+  } else if (answerOne === 'yes' || answerOne === "y") {
+    alert("You're wrong " + userName + "! I was born in South Dakota.");
+  } else {
+    alert("That was not a valid response. You still get it wrong!");
+  }
+};
 
-if (questionOne === 'no' || questionOne === "n") {
-  alert("Good Job " + userName + "! You're off to a good start.");
-} else if (questionOne === 'yes' || questionOne === "y") {
-  alert("You're wrong " + userName + "! I was born in South Dakota.");
-} else {
-  alert("That was not a valid response. You still get it wrong!");
-}
+var questionTwo = function() {
+  var answerTwo = prompt("Is one of my favorite pastimes skateboarding? (Please answer yes or no)").toLowerCase();
+  if (answerTwo === 'yes' || answerTwo === "y") {
+    alert("Correct " + userName + "! I once broke both bones in my left arm at the same time!");
+  } else if (answerTwo === 'no' || answerTwo === "n") {
+    alert("Wrong! I don't skate as much anymore but I still love to ride around.");
+  } else {
+    alert("That was not a valid response " + userNamer + ". You still get it wrong!");
+  }
+};
 
-var questionTwo = prompt("Is one of my favorite pastimes skateboarding? (Please answer yes or no)").toLowerCase();
+var questionThree = function() {
+  var answerThree = prompt("Do I own lots of board games? (Please answer yes or no)").toLowerCase();
+  if (answerThree=== 'yes' || answerThree === "y") {
+    alert("Yes! I own many board games. My favorite one is Arkham Horror! We should play sometime " + userName + ".");
+  } else if (answerThree === 'no' || answerThree === "n") {
+    alert("Wrong " + userName + "! I have a whole bookshelf full of them!");
+  } else {
+    alert("That was not a valid response. You still get it wrong" + userName + "!");
+  }
+};
 
-if (questionTwo === 'yes' || questionTwo === "y") {
-  alert("Correct " + userName + "! I once broke both bones in my left arm at the same time!");
-} else if (questionTwo === 'no' || questionTwo === "n") {
-  alert("Wrong! I don't skate as much anymore but I still love to ride around.");
-} else {
-  alert("That was not a valid response " + userNamer + ". You still get it wrong!");
-}
+var questionFour = function() {
+  var answerFour = prompt("Have I ever met a celebrity? (Please answer yes or no)").toLowerCase();
+  if (answerFour=== 'yes' || answerFour === "y") {
+    alert("Great work " + userName + "! I once met Prince on a plane!");
+  } else if (answerFour === 'no' || answerFour === "n") {
+    alert("Incorrect " + userName+ ". You will have to get it right to see who it was!");
+  } else {
+    alert("That was not a valid response. You still get it wrong!");
+  }
+};
 
-var questionThree = prompt("Do I own lots of board games? (Please answer yes or no)").toLowerCase();
+var main = function() {
+  questionOne();
+  questionTwo();
+  questionThree();
+  questionFour();
+};
 
-if (questionThree=== 'yes' || questionThree === "y") {
-  alert("Yes! I own many board games. My favorite one is Arkham Horror! We should play sometime " + userName + ".");
-} else if (questionThree === 'no' || questionThree === "n") {
-  alert("Wrong" + userName + "! I have a whole bookshelf full of them!");
-} else {
-  alert("That was not a valid response. You still get it wrong" + userName + "!");
-}
-
-var questionFour = prompt("Have I ever met a celebrity? (Please answer yes or no)").toLowerCase();
-
-if (questionFour=== 'yes' || questionFour === "y") {
-  alert("Great work " + userName + "! I once met Prince on a plane!");
-} else if (questionThree === 'no' || questionThree === "n") {
-  alert("Incorrect " + userName+ ". You will have to get it right to see who it was!");
-} else {
-  alert("That was not a valid response. You still get it wrong!");
-}
+main();
