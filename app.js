5115fe256cf08a79bcc93cb57d6a650b81806fae
Kyle Winckler <kyle.winckler@gmail.com>
2016-05-26 20:37:44 -0700

    added userName variable and personalized alert messages

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+var userName = prompt("Identify yourself user?"); // This will get the users name
+
 var questionOne = prompt("Was I born in North Dakota? (Please answer yes or no)").toLowerCase();
 
 if (questionOne === 'no' || questionOne === "n") {
-  alert("Good Job! You're off to a good start.");
+  alert("Good Job " + userName + "! You're off to a good start.");
 } else if (questionOne === 'yes' || questionOne === "y") {
-  alert("Wrong! I was born in South Dakota.");
+  alert("You're wrong " + userName + "! I was born in South Dakota.");
 } else {
   alert("That was not a valid response. You still get it wrong!");
 }
@@ -11,29 +13,29 @@ if (questionOne === 'no' || questionOne === "n") {
 var questionTwo = prompt("Is one of my favorite pastimes skateboarding? (Please answer yes or no)").toLowerCase();
 
 if (questionTwo === 'yes' || questionTwo === "y") {
-  alert("Correct! I once broke both bones in my left arm at the same time!");
+  alert("Correct " + userName + "! I once broke both bones in my left arm at the same time!");
 } else if (questionTwo === 'no' || questionTwo === "n") {
   alert("Wrong! I don't skate as much anymore but I still love to ride around.");
 } else {
-  alert("That was not a valid response. You still get it wrong!");
+  alert("That was not a valid response " + userNamer + ". You still get it wrong!");
 }
 
 var questionThree = prompt("Do I own lots of board games? (Please answer yes or no)").toLowerCase();
 
 if (questionThree=== 'yes' || questionThree === "y") {
-  alert("Yes! I own many board games. My favorite one is Arkham Horror!");
+  alert("Yes! I own many board games. My favorite one is Arkham Horror! We should play sometime " + userName + ".");
 } else if (questionThree === 'no' || questionThree === "n") {
-  alert("Wrong! I have a whole bookshelf full of them!");
+  alert("Wrong" + userName + "! I have a whole bookshelf full of them!");
 } else {
-  alert("That was not a valid response. You still get it wrong!");
+  alert("That was not a valid response. You still get it wrong" + userName + "!");
 }
 
 var questionFour = prompt("Have I ever met a celebrity? (Please answer yes or no)").toLowerCase();
 
 if (questionFour=== 'yes' || questionFour === "y") {
-  alert("Great work! I once met Prince on a plane!");
+  alert("Great work " + userName + "! I once met Prince on a plane!");
 } else if (questionThree === 'no' || questionThree === "n") {
-  alert("Incorrect. You will have to get it right to see who it was!");
+  alert("Incorrect " + userName+ ". You will have to get it right to see who it was!");
 } else {
   alert("That was not a valid response. You still get it wrong!");
 }
