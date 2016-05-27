a35a7914c45ab5f9abe1eced90f44b8f19a2b21a
Kyle Winckler <kyle.winckler@gmail.com>
2016-05-26 18:17:49 -0700

    changed question logic - added second question

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 var questionOne = prompt("Was I born in North Dakota?").toLowerCase();
 
-if (questionOne === 'yes' || questionOne === "y") {
+if (questionOne === 'no' || questionOne === "n") {
+  alert("Good Job! You're off to a good start.");
+} else if (questionOne === 'yes' || questionOne === "y") {
   alert("Wrong! I was born in South Dakota.");
 } else {
-  alert("Good Job! You're off to a good start.");
+  alert("That was not a valid response. You still get it wrong!");
+}
+
+var questionTwo = prompt("Is one of my favorite pastimes skateboarding?").toLowerCase();
+
+if (questionOne === 'yes' || questionOne === "y") {
+  alert("Correct! I once broke both bones in my left arm at the same time!");
+} else if (questionOne === 'no' || questionOne === "n") {
+  alert("Wrong! I don't skate as much anymore but I still love to ride around.");
+} else {
+  alert("That was not a valid response. You still get it wrong!");
 }
