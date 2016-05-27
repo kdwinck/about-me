af25017a581dbc6da0473eeb45e0ec56accf2179
Kyle Winckler <kyle.winckler@gmail.com>
2016-05-26 18:31:48 -0700

    added third and fourth questions

--- app.js
+++ app.js
@@ -10,10 +10,30 @@ if (questionOne === 'no' || questionOne === "n") {
 
 var questionTwo = prompt("Is one of my favorite pastimes skateboarding?").toLowerCase();
 
-if (questionOne === 'yes' || questionOne === "y") {
+if (questionTwo === 'yes' || questionTwo === "y") {
   alert("Correct! I once broke both bones in my left arm at the same time!");
-} else if (questionOne === 'no' || questionOne === "n") {
+} else if (questionTwo === 'no' || questionTwo === "n") {
   alert("Wrong! I don't skate as much anymore but I still love to ride around.");
+} else {
+  alert("That was not a valid response. You still get it wrong!");
+}
+
+var questionThree = prompt("Do I own a lot of board games?").toLowerCase();
+
+if (questionThree=== 'yes' || questionThree === "y") {
+  alert("Yes! I own many board games. My favorite one is Arkham Horror!");
+} else if (questionThree === 'no' || questionThree === "n") {
+  alert("Wrong! I have a whole bookshelf full of them!");
+} else {
+  alert("That was not a valid response. You still get it wrong!");
+}
+
+var questionFour = prompt("Have I ever met a celebrity?").toLowerCase();
+
+if (questionFour=== 'yes' || questionFour === "y") {
+  alert("Great work! I once met Prince on a plane!");
+} else if (questionThree === 'no' || questionThree === "n") {
+  alert("Incorrect. You will have to get it right to see who it was!");
 } else {
   alert("That was not a valid response. You still get it wrong!");
 }
