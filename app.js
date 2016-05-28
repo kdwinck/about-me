cdab6f17e6f80f68a5faf02381ac028ef9abcfb0
Kyle Winckler <kyle.winckler@gmail.com>
2016-05-28 13:14:04 -0700

    added score variable and display score function

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-var userName = prompt("Identify yourself user?"); // This will get the users name
+var userName = prompt("Identify yourself user."); // This will get the users name
+var score = 0;
 
 var questionOne = function() {
   var answerOne = prompt("Was I born in North Dakota? (Please answer yes or no)").toLowerCase();
   if (answerOne === 'no' || answerOne === "n") {
     alert("Good Job " + userName + "! You're off to a good start.");
+    score ++;
   } else if (answerOne === 'yes' || answerOne === "y") {
     alert("You're wrong " + userName + "! I was born in South Dakota.");
   } else {
@@ -16,6 +18,7 @@ var questionTwo = function() {
   var answerTwo = prompt("Is one of my favorite pastimes skateboarding? (Please answer yes or no)").toLowerCase();
   if (answerTwo === 'yes' || answerTwo === "y") {
     alert("Correct " + userName + "! I once broke both bones in my left arm at the same time!");
+    score ++;
   } else if (answerTwo === 'no' || answerTwo === "n") {
     alert("Wrong! I don't skate as much anymore but I still love to ride around.");
   } else {
@@ -28,6 +31,7 @@ var questionThree = function() {
   var answerThree = prompt("Do I own lots of board games? (Please answer yes or no)").toLowerCase();
   if (answerThree=== 'yes' || answerThree === "y") {
     alert("Yes! I own many board games. My favorite one is Arkham Horror! We should play sometime " + userName + ".");
+    score ++;
   } else if (answerThree === 'no' || answerThree === "n") {
     alert("Wrong " + userName + "! I have a whole bookshelf full of them!");
   } else {
@@ -40,6 +44,7 @@ var questionFour = function() {
   var answerFour = prompt("Have I ever met a celebrity? (Please answer yes or no)").toLowerCase();
   if (answerFour=== 'yes' || answerFour === "y") {
     alert("Great work " + userName + "! I once met Prince on a plane!");
+    score ++;
   } else if (answerFour === 'no' || answerFour === "n") {
     alert("Incorrect " + userName+ ". You will have to get it right to see who it was!");
   } else {
@@ -48,11 +53,16 @@ var questionFour = function() {
   }
 };
 
+var displayScore = function() {
+  alert("Your score was " + score + " out of 4. How did you do " + userName + "?");
+};
+
 var main = function() {
   questionOne();
   questionTwo();
   questionThree();
   questionFour();
+  displayScore();
 };
 
 main();
