e6dedb59e24cc1b5c1d72a5aebcc170cb04e7da7
Kyle Winckler <kyle.winckler@gmail.com>
2016-05-31 17:17:46 -0700

    created loop for all yes/no questions

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ for (var i = 0; i < questions.length; i++) {
   }
 }
 
+// previously had 4 functions similar to the following -- replaced by loop above
+
 // var questionOne = function() {
 //   var answerOne = prompt("Was I born in North Dakota? (Please answer yes or no)").toLowerCase();
 //   if (answerOne === 'no' || answerOne === "n") {
@@ -37,45 +39,6 @@ for (var i = 0; i < questions.length; i++) {
 //   } else {
 //     alert("Please input a valid response. (y)es or (n)o");
 //     questionOne();
-//   }
-// };
-//
-// var questionTwo = function() {
-//   var answerTwo = prompt("Is one of my favorite pastimes skateboarding? (Please answer yes or no)").toLowerCase();
-//   if (answerTwo === 'yes' || answerTwo === "y") {
-//     alert("Correct " + userName + "! I once broke both bones in my left arm at the same time!");
-//     score ++;
-//   } else if (answerTwo === 'no' || answerTwo === "n") {
-//     alert("Wrong! I don't skate as much anymore but I still love to ride around.");
-//   } else {
-//     alert("Please input a valid response. (y)es or (n)o");
-//     questionTwo();
-//   }
-// };
-//
-// var questionThree = function() {
-//   var answerThree = prompt("Do I own lots of board games? (Please answer yes or no)").toLowerCase();
-//   if (answerThree=== 'yes' || answerThree === "y") {
-//     alert("Yes! I own many board games. My favorite one is Arkham Horror! We should play sometime " + userName + ".");
-//     score ++;
-//   } else if (answerThree === 'no' || answerThree === "n") {
-//     alert("Wrong " + userName + "! I have a whole bookshelf full of them!");
-//   } else {
-//     alert("Please input a valid response. (y)es or (n)o");
-//     questionThree();
-//   }
-// };
-//
-// var questionFour = function() {
-//   var answerFour = prompt("Have I ever met a celebrity? (Please answer yes or no)").toLowerCase();
-//   if (answerFour=== 'yes' || answerFour === "y") {
-//     alert("Great work " + userName + "! I once met Prince on a plane!");
-//     score ++;
-//   } else if (answerFour === 'no' || answerFour === "n") {
-//     alert("Incorrect " + userName+ ". You will have to get it right to see who it was!");
-//   } else {
-//     alert("Please input a valid response. (y)es or (n)o");
-//     questionFour();
 //   }
 // };
 
@@ -118,10 +81,6 @@ var displayScore = function() {
 };
 
 var main = function() {
-  // questionOne();
-  // questionTwo();
-  // questionThree();
-  // questionFour();
   questionFive();
   questionSix();
   displayScore();
