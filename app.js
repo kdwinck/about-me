18cfc96086c97a422c95260a19134277ae904fb7
Kyle Winckler <kyle.winckler@gmail.com>
2016-06-02 17:38:04 -0700

    moved yes/no arrays into yes/no questions function

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 var userName;
 var score = 0;
 
-var questions = [
-  'Was I born in North Dakota?',
-  'Is one of my favorite pastimes skateboarding?',
-  'Do I own a lot of board games?',
-  'Have I ever met a celebrity?'
-];
-
-var answers = ['no', 'yes', 'yes', 'yes'];
-
-var responses = [
-                  ['Good Job ' + userName + '! You\'re off to a good start.', 'You\'re wrong ' + userName + '! I was born in South Dakota.'],
-                  ['Correct ' + userName + '! I once broke both bones in my left arm at the same time!', 'Wrong! I don\'t skate as much anymore but I still love to ride around.'],
-                  ['Yes! I own many board games. My favorite one is Arkham Horror! We should play sometime ' + userName + '.', 'Wrong ' + userName + '! I have a whole bookshelf full of them!'],
-                  ['Great work ' + userName + '! I once met Prince on a plane!', 'Incorrect ' + userName + '. You will have to get it right to see who it was!']
-];
-
 function getUserName() {
   userName = prompt('Identify yourself user.'); // This will get the users name
 }
 
 function askYesNoQuestions() {
+
+  var questions = [
+    'Was I born in North Dakota?',
+    'Is one of my favorite pastimes skateboarding?',
+    'Do I own a lot of board games?',
+    'Have I ever met a celebrity?'
+  ];
+
+  var answers = ['no', 'yes', 'yes', 'yes'];
+
+  var responses = [
+                    ['Good Job ' + userName + '! You\'re off to a good start.', 'You\'re wrong ' + userName + '! I was born in South Dakota.'],
+                    ['Correct ' + userName + '! I once broke both bones in my left arm at the same time!', 'Wrong! I don\'t skate as much anymore but I still love to ride around.'],
+                    ['Yes! I own many board games. My favorite one is Arkham Horror! We should play sometime ' + userName + '.', 'Wrong ' + userName + '! I have a whole bookshelf full of them!'],
+                    ['Great work ' + userName + '! I once met Prince on a plane!', 'Incorrect ' + userName + '. You will have to get it right to see who it was!']
+  ];
+
   for (var i = 0; i < questions.length; i++) {
     var answer = prompt(questions[i]).toLowerCase();
     if (answer === answers[i] || answer === answers[i].charAt(0)) {
@@ -66,8 +67,8 @@ function questionSix() {
     }
   }
   var allAnswers = '';
-  for (var i = 0; i < answers.length; i++) {
-    allAnswers += answers[i] + ' ,';
+  for (var i = 0; i < movies.length; i++) {
+    allAnswers += movies[i] + ', ';
   }
   alert('The options were ' + allAnswers);
 };
