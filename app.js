e48a519a4cf0d463601bae3fdc8e46ca0f067262
Kyle Winckler <kyle.winckler@gmail.com>
2016-06-02 17:58:30 -0700

    simplify for loops and add comments

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-var userName;
-var score = 0;
+var userName;    //declare variable for userName
+var score = 0;   // define starting user score
 
 function getUserName() {
-  userName = prompt('Identify yourself user.'); // This will get the users name
+  userName = prompt('Identify yourself user.');
 }
 
 function askYesNoQuestions() {
 
-  var questions = [
+  var questions = [                   // declare yes/no questions
     'Was I born in North Dakota?',
     'Is one of my favorite pastimes skateboarding?',
     'Do I own a lot of board games?',
     'Have I ever met a celebrity?'
   ];
 
-  var answers = ['no', 'yes', 'yes', 'yes'];
+  var answers = ['no', 'yes', 'yes', 'yes'];   // declare yes/no answers
 
+  // declare responses if right or wrong
   var responses = [
                     ['Good Job ' + userName + '! You\'re off to a good start.', 'You\'re wrong ' + userName + '! I was born in South Dakota.'],
                     ['Correct ' + userName + '! I once broke both bones in my left arm at the same time!', 'Wrong! I don\'t skate as much anymore but I still love to ride around.'],
@@ -23,21 +24,22 @@ function askYesNoQuestions() {
                     ['Great work ' + userName + '! I once met Prince on a plane!', 'Incorrect ' + userName + '. You will have to get it right to see who it was!']
   ];
 
-  for (var i = 0; i < questions.length; i++) {
-    var answer = prompt(questions[i]).toLowerCase();
-    if (answer === answers[i] || answer === answers[i].charAt(0)) {
-      alert(responses[i][0]);
+  // loop through questions, check answer, alert with appropriate response
+  for (value in questions) {
+    var answer = prompt(questions[value]).toLowerCase();
+    if (answer === answers[value] || answer === answers[value].charAt(0)) {
+      alert(responses[value][0]);
       score ++;
     } else {
-      alert(responses[i][1]);
+      alert(responses[value][1]);
     }
   }
 };
 
 function questionFive() {
-  var guesses = 4;
+  var guesses = 4;  // declare number of guesses
   while (guesses > 0) {
-    var answerFive = parseInt(prompt('What is my favorite number?'));
+    var answerFive = parseInt(prompt('What is my favorite number?'));  // convert answer to an integer
     if (answerFive > 17) {
       alert('Your guess was too high.');
       guesses --;
@@ -59,16 +61,17 @@ function questionSix() {
     var answerSix = prompt('What is one of my favorite movies?').toLowerCase();
     if (movies.indexOf(answerSix) !== -1) {
       alert('Wow ' + userName + '! Great Job');
-      guesses = 0;
+      guesses = 0;  // after correct answer set break loop by setting guesses to 0
       score ++;
     } else {
       alert('Try again!');
-      guesses --;
+      guesses --;  // subtract from number of guesses remaining
     }
   }
   var allAnswers = '';
-  for (var i = 0; i < movies.length; i++) {
-    allAnswers += movies[i] + ', ';
+  // list all posible answers
+  for (value in movies) {
+    allAnswers += movies[value] + ', ';
   }
   alert('The options were ' + allAnswers);
 };
@@ -76,7 +79,7 @@ function questionSix() {
 function displayScore() {
   alert('Your score was ' + score + ' out of 6. How did you do ' + userName + '?');
   var el = document.getElementById('display-score');
-  el.textContent = 'Score: ' + score;
+  el.textContent = 'Score: ' + score;  // update element to display score on page
 };
 
 function main() {
